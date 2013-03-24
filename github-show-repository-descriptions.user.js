a7be666d953fd1db38a510934b8894076bafcc01
vzvu3k6k <vzvu3k6k@gmail.com>
2013-03-24 19:42:18 +0900

    Fix typo

--- github-show-repository-descriptions.user.js
+++ github-show-repository-descriptions.user.js
@@ -9,7 +9,7 @@
 // ==/UserScript==
 
 // This script should work in
-//   * https://github.com/<organize> (eg. https://github.com/github)
+//   * https://github.com/<organization> (eg. https://github.com/github)
 //   * https://github.com/<user>?tab=repositories(#) (eg. https://github.com/mojombo?tab=repositories)
 
 (function(){
