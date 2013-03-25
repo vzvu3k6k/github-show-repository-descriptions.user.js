2e4e6e9b7953f4350143032959f11d5993c792d4
vzvu3k6k <vzvu3k6k@gmail.com>
2013-03-26 00:01:13 +0900

    Fix description in metablock

--- github-show-repository-descriptions.user.js
+++ github-show-repository-descriptions.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Github: Read all repository descriptions in user's repositories page
-// @description    Read all descriptions in https://github.com/{user}?tab=repositories, https://github.com/{organization}
+// @description    Read all descriptions in Github repository list page
 // @version        1.0
 // @author         vzvu3k6k
 // @match          https://github.com/*
